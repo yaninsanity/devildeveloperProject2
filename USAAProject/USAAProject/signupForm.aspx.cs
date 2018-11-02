9cf53aa4faa956e934d45fa39c070ed8c3f0115b
Zeren Yan <zyan30@sundevils.asu.edu>
2018-11-02 01:13:04 -0700

    webconfig setup

--- USAAProject/USAAProject/signupForm.aspx.cs
+++ USAAProject/USAAProject/signupForm.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace USAAProject
 {
@@ -12,6 +15,36 @@ namespace USAAProject
         protected void Page_Load(object sender, EventArgs e)
         {
 
+
+
+
+
+
         }
-    }
+        private void addNewAcc()
+         {
+
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+			string sqlSelect = "insert into useraccount (username, password) values (@uid, @pass);";
+
+			SqlConnection sqlConnection = new SqlConnection(sqlConnectString);
+			SqlCommand sqlCommand = new SqlCommand(sqlSelect, sqlConnection);
+
+			sqlCommand.Parameters.Add("@uid", System.Data.SqlDbType.NVarChar);
+			sqlCommand.Parameters["@uid"].Value = idBox.Text;
+			sqlCommand.Parameters.Add("@pass", System.Data.SqlDbType.NVarChar);
+			sqlCommand.Parameters["@pass"].Value = pwdBox.Text;
+
+			sqlConnection.Open();
+			sqlCommand.ExecuteNonQuery();
+			sqlConnection.Close();
+
+			idBox.Text = "";
+			pwdBox.Text = "";
+			userView.DataBind();
+         }
+    
+        
+}
+
 }
