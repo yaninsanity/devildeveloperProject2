450b58b044f526d2ba3fdf0d35096c1ca4c55d93
Zeren Yan <zyan30@sundevils.asu.edu>
2018-11-05 01:07:47 -0700

    signin

---
+++ USAAProject/USAAProject/SignIn.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/USAADevilDevelopers.Master" AutoEventWireup="true" CodeBehind="SignIn.aspx.cs" Inherits="USAAProject.SignIn" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table style="width:100%;">
+        <tr>
+            <td class="text-center" colspan="4">Sign In</td>
+        </tr>
+        <tr>
+            <td style="width: 150px">&nbsp;</td>
+            <td style="width: 199px">username</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td style="width: 150px">&nbsp;</td>
+            <td style="width: 199px">
+                <asp:TextBox ID="userAccTextBox" runat="server"></asp:TextBox>
+            </td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td style="width: 150px">&nbsp;</td>
+            <td style="width: 199px">password</td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+        </tr>
+        <tr>
+            <td style="width: 150px; height: 25px"></td>
+            <td style="width: 199px; height: 25px">
+                <asp:TextBox ID="pswdTextBox" textMode="Password" runat="server"></asp:TextBox>
+            </td>
+            <td style="height: 25px"></td>
+            <td style="height: 25px"></td>
+        </tr>
+        <tr>
+            <td style="width: 150px; height: 25px">&nbsp;</td>
+            <td style="width: 199px; height: 25px">
+                &nbsp;</td>
+            <td style="height: 25px">&nbsp;</td>
+            <td style="height: 25px">&nbsp;</td>
+        </tr>
+        <tr>
+            <td style="width: 150px; height: 26px">&nbsp;</td>
+            <td style="width: 199px; height: 26px">
+                <asp:Button ID="signInButton" runat="server"  Text="Sign In" Width="178px" />
+            </td>
+            <td style="height: 26px">&nbsp;</td>
+            <td style="height: 26px">&nbsp;</td>
+        </tr>
+        <tr>
+            <td style="height: 26px" colspan="4">
+                <asp:Label ID="errorLabel" runat="server" ForeColor="Red" Visible="False"></asp:Label>
+            </td>
+        </tr>
+    </table>
+</asp:Content>
---
+++ USAAProject/USAAProject/SignIn.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Web.Configuration;
+
+namespace USAAProject
+{
+    public partial class SignIn : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //pass into userAccount and user password
+        protected void checkAcc(string acc,string pwd)
+        {
+            //store correct password
+            String correctPswd="";
+            bool accountNameWrong = true;
+            string tmpString = "";
+            bool Error = false;
+            String errorMSG="";
+
+            //Connect to the DB
+
+            string connString = WebConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            string sqlsearch= $"select empPassword,count(empID) founded from employee where empID={acc} OR empEmail={acc}";
+
+            MySqlCommand cmd = new MySqlCommand(sqlsearch, conn);
+
+            conn.Open();
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            
+
+            if (dr.Read())
+            {
+                correctPswd = (String)dr["empPassword"].ToString().Trim();
+                tmpString = (String)dr["founded"].ToString().Trim();
+                if (tmpString == "0")
+                {
+                    Error = true;
+                    errorMSG = "Account not founded, Please try another employee ID or employee Email";
+                }
+                else
+                {
+                    if (pwd != correctPswd)
+                    {
+                        errorMSG = "Please try another password.";
+                        Error = true;
+                    }
+                    else
+                    {
+                        // Session["UID"]
+                        Response.Redirect("userProfile.aspx");
+                    }
+
+                }
+            }
+            conn.Close();
+            if (Error)
+
+            {
+                errorLabel.Text= errorMSG;
+                errorLabel.Visible = true;
+            }
+
+
+        }
+    }
+}
